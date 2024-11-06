2188074fc5490562ff0bb48c6d097ae3c4936ad6
Pasindu <pasindusandakan@gmail.com>
2024-11-06 11:42:56 +0530

    initiate commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Product_Count_API
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="prodcutCount",
+    version="0.0.0",
+    author="pasindu",
+    author_email="",
+    packages=find_packages(),
+    install_requires=[],
+)
